62157fb424afaa9301596a83741f19f58594a40b
White <74352995+White-145@users.noreply.github.com>
2022-02-01 20:45:35 +0400

    added comments

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ from pynput import keyboard
 import os
 import main
 
+# Functions used to move player entities
 def translate_keys(func):
     keys_up = {keyboard.Key.up, main.key('w')}
     keys_down = {keyboard.Key.down, main.key('s')}
@@ -37,17 +38,21 @@ def move(entity, key):
     area.show()
     print(f'\r{area}', end='')
 
+# Setup area
 area = main.Area(width=20, height=20, texture='-', layer=0)
 
+# Create entities
 player_1 = main.Entity(area, texture='@', y=10, x=10, width=3, height=2)
 player_2 = main.Entity(area, texture='%', layer=1, y=5, x=10, width=3, height=3)
 
 wall = main.Entity(area, texture='/', layer=2, y=2, x=2, width=4, height=4)
-platform = main.Entity(area, texture='\\', layer=-1, y=4, x=4, width=4, height=4)
+platform = main.Entity(area, texture='\\', layer=-1, y=4, x=4, width=4, height=4)  # This entity will be hidden due of highter area layer
 
+# Show area and area info
 area.show()
 print(f'\r{area}', end='')
 
+# Move players
 keys_1 = {
     keyboard.Key.up,
     keyboard.Key.down,
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from pynput import keyboard
 import os
 
-key = lambda key: keyboard.KeyCode.from_char(key)
+key = lambda key: keyboard.KeyCode.from_char(key)  # function to get text key
 
+# get argument
 def set_arg(arg, args, rtype, default):
     argument = args.pop(arg, None)
     if argument is not None:
@@ -12,33 +13,36 @@ def set_arg(arg, args, rtype, default):
         return default
     return argument
 
+# string to texture
+def fix_texture(texture):
+    if texture == '':
+        return '_'
+    elif texture[0] in {'\a', '\b', '\f', '\n', '\r', '\t', '\v'}:
+        return '_'
+    else:
+        return texture[0]
+
 class Area:
-    areas = []
+    areas = []  # list of all areas
 
     def __init__(self, **kwargs):
+        # throw error if extra args given
         if list(filter(lambda x: x not in {"width", "height", "layer", "texture"}, kwargs)):
             raise TypeError("invalid keyword arguments to Area()")
-        
-        def fix_texture(texture):
-            if texture == '':
-                return '_'
-            elif texture[0] in {'\a', '\b', '\f', '\n', '\r', '\t', '\v'}:
-                return '_'
-            else:
-                return texture[0]
 
         clamp = lambda x, l: max(1, min(x, l))
 
+        # set variables
         self.width = clamp(set_arg("width", kwargs, int, 5), 120)
         self.height = clamp(set_arg("height", kwargs, int, 5), 29)
         self.layer = set_arg('layer', kwargs, int, None)
         self.texture = fix_texture(set_arg('texture', kwargs, str, '_'))
 
-        self.entities = []
-        self.__class__.areas.append(self)
+        self.entities = []  # all entities on area
+        self.__class__.areas.append(self)  # add area to all areas
 
     def show(self):
-        os.system('cls')
+        os.system('cls')  # clear terminal (windows)
 
         area_full = [[self.texture for _ in range(self.width)] for _ in range(self.height)]
         area_layers = [[self.layer for _ in range(self.width)] for _ in range(self.height)]
@@ -59,51 +63,37 @@ class Area:
         return f'area {self.texture} ID {self.__class__.areas.index(self)} with {len(self.entities)} entities'
 
 class Entity:
-    entities = []
+    entities = []  # list of all entities
 
     def __init__(self, area, **kwargs):
+        # throw error if area not given
         if not isinstance(area, Area):
             raise TypeError("area must be instance of Area class")
 
+        # throw error if extra args given
         if list(filter(lambda x: x not in {"texture", "layer", 'y', 'x', "width", "height"}, kwargs)):
             raise TypeError("invalid keyword arguments to Entity()")
 
-        texture = set_arg("texture", kwargs, str, '#')
-        layer = set_arg("layer", kwargs, int, 0)
-        y = set_arg('y', kwargs, int, 0)
-        x = set_arg('x', kwargs, int, 0)
-        width = set_arg('width', kwargs, int, 1)
-        height = set_arg('height', kwargs, int, 1)
+        limit = lambda x: 1 if x < 1 else x
 
-        if texture == '':
-            self.texture = '#'
-        else:
-            self.texture = texture[0]
+        # set variables
+        self.texture = fix_texture(set_arg("texture", kwargs, str, '#'))
+        self.layer = set_arg("layer", kwargs, int, 0)
+        self.y = set_arg('y', kwargs, int, 0)
+        self.x = set_arg('x', kwargs, int, 0)
+        self.width = limit(set_arg('width', kwargs, int, 1))
+        self.height = limit(set_arg('height', kwargs, int, 1))
 
-        if width < 1:
-            width = 1
+        area.entities.append(self)  # add entity to area
+        self.__class__.entities.append(self)  # add entity to all entities
 
-        if height < 1:
-            height = 1
-
-        self.y = y
-        self.x = x
-
-        self.area = area
-        self.layer = layer
-
-        self.width = width
-        self.height = height
-
-        area.entities.append(self)
-        self.__class__.entities.append(self)
-
+    # get all covers
     @property
     def covers(self):
-        is_in_area = lambda loc: 0 <= loc[0] < self.area.height and 0 <= loc[1] < self.area.width
+        is_on_area = lambda loc: 0 <= loc[0] < self.area.height and 0 <= loc[1] < self.area.width
 
         all_covers = [(self.y + y_offset, self.x + x_offset) for y_offset in range(self.height) for x_offset in range(self.width)]
-        covers = tuple(filter(is_in_area, all_covers))
+        covers = tuple(filter(is_on_area, all_covers))
 
         return covers
 
